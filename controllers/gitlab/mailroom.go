e373bb808edacad29093a3356a7cb565f258a8a0
Gerard Hickey <ghickey@gitlab.com>
2021-09-01 19:57:44 -0700

    Added methods for additonal Mailroom objs

--- controllers/gitlab/mailroom.go
+++ controllers/gitlab/mailroom.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 
 	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv2beta1 "k8s.io/api/autoscaling/v2beta1"
 	corev1 "k8s.io/api/core/v1"
+	networkpolicyv1 "k8s.io/api/networking/v1"
 )
 
-// MailroomService returns the Service for the Mailroom component.
-func MailroomService(adapter CustomResourceAdapter) *corev1.Service {
-	template, err := GetTemplate(adapter)
-	if err != nil {
-		return nil // WARNING: this should return an error
-	}
-	result := template.Query().ServiceByComponent(MailroomComponentName)
+const (
+	globalMailroomEnabled  = "global.mailroom.enabled"
+	mailroomEnabledDefault = false
+)
 
-	return result
+// MailroomEnabled returns `true` if enabled and `false` if not.
+func MailroomEnabled(adapter CustomResourceAdapter) bool {
+	enabled, _ := GetBoolValue(adapter.Values(), globalMailroomEnabled, mailroomEnabledDefault)
+	return enabled
 }
 
 // MailroomDeployment returns the Deployment for the Mailroom component.
@@ -30,8 +32,8 @@ func MailroomDeployment(adapter CustomResourceAdapter) *appsv1.Deployment {
 	return result
 }
 
-// MailroomConfigMaps returns the ConfigMaps for the Mailroom component.
-func MailroomConfigMaps(adapter CustomResourceAdapter) []*corev1.ConfigMap {
+// MailroomConfigMapsreturns the ConfigMaps for the Mailroom component.
+func MailroomConfigMap(adapter CustomResourceAdapter) *corev1.ConfigMap {
 	template, err := GetTemplate(adapter)
 	if err != nil {
 		return nil // WARNING: this should return an error
@@ -42,5 +44,50 @@ func MailroomConfigMaps(adapter CustomResourceAdapter) []*corev1.ConfigMap {
 
 	result := []*corev1.ConfigMap{exporterCfgMap}
 
+	return result[0]
+}
+
+// MailroomHPA returns the HPA for the Mailroom component.
+func MailroomHPA(adapter CustomResourceAdapter) *autoscalingv2beta1.HorizontalPodAutoscaler {
+	template, err := GetTemplate(adapter)
+	if err != nil {
+		return nil // WARNING: this should return an error
+	}
+
+	hpa := template.Query().HPAByName(
+		fmt.Sprintf("%s-%s", adapter.ReleaseName(), MailroomComponentName))
+
+	result := []*autoscalingv2beta1.HorizontalPodAutoscaler{hpa}
+
+	return result[0]
+}
+
+// MailroomNetworkPolicy returns the NetworkPolicy for the Mailroom component.
+func MailroomNetworkPolicy(adapter CustomResourceAdapter) []*networkpolicyv1.NetworkPolicy {
+	template, err := GetTemplate(adapter)
+	if err != nil {
+		return nil // WARNING: this should return an error
+	}
+
+	policy := template.Query().NetworkPolicyByName(
+		fmt.Sprintf("%s-%s", adapter.ReleaseName(), MailroomComponentName))
+
+	result := []*networkpolicyv1.NetworkPolicy{policy}
+
 	return result
+}
+
+// MailroomServiceAccount returns the ServiceAccount for the Mailroom component.
+func MailroomServiceAccount(adapter CustomResourceAdapter) *corev1.ServiceAccount {
+	template, err := GetTemplate(adapter)
+	if err != nil {
+		return nil // WARNING: this should return an error
+	}
+
+	account := template.Query().ServiceAccountByName(
+		fmt.Sprintf("%s-%s", adapter.ReleaseName(), MailroomComponentName))
+
+	result := []*corev1.ServiceAccount{account}
+
+	return result[0]
 }

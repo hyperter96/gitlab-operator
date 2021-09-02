149ac357db2bae647faa9d82d5cf8f3793956c40
Gerard Hickey <ghickey@gitlab.com>
2021-09-01 19:58:58 -0700

    Added basic Mailroom testing

---
+++ controllers/gitlab/mailroom_test.go
@@ -0,0 +1,77 @@
+package gitlab
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"gitlab.com/gitlab-org/cloud-native/gitlab-operator/helm"
+)
+
+var _ = Describe("CustomResourceAdapter", func() {
+
+	if namespace == "" {
+		namespace = "default"
+	}
+
+	Context("Mailroom", func() {
+		When("Mailroom is enabled", func() {
+			chartValues := helm.EmptyValues()
+			_ = chartValues.SetValue(globalMailroomEnabled, true)
+
+			adapter := createMockAdapter(namespace, chartVersions[0], chartValues)
+			template, err := GetTemplate(adapter)
+
+			enabled := MailroomEnabled(adapter)
+			configMap := MailroomConfigMap(adapter)
+			hpa := MailroomHPA(adapter)
+			deployment := MailroomDeployment(adapter)
+			networkPolicy := MailroomNetworkPolicy(adapter)
+			serviceAccount := MailroomServiceAccount(adapter)
+
+			It("Should render the template", func() {
+				Expect(err).To(BeNil())
+				Expect(template).NotTo(BeNil())
+			})
+
+			It("Should contain Mailroom resources", func() {
+				Expect(enabled).To(BeTrue())
+				Expect(deployment).NotTo(BeNil())
+				Expect(configMap).NotTo(BeNil())
+				Expect(hpa).NotTo(BeNil())
+				Expect(networkPolicy).NotTo(BeNil())
+				Expect(serviceAccount).NotTo(BeNil())
+			})
+
+		})
+
+		When("Mailroom is disabled", func() {
+			chartValues := helm.EmptyValues()
+			_ = chartValues.SetValue(globalMailroomEnabled, false)
+
+			adapter := createMockAdapter(namespace, chartVersions[0], chartValues)
+			template, err := GetTemplate(adapter)
+
+			enabled := MailroomEnabled(adapter)
+			configMap := MailroomConfigMap(adapter)
+			hpa := MailroomHPA(adapter)
+			deployment := MailroomDeployment(adapter)
+			networkPolicy := MailroomNetworkPolicy(adapter)
+			serviceAccount := MailroomServiceAccount(adapter)
+
+			It("Should render the template", func() {
+				Expect(err).To(BeNil())
+				Expect(template).NotTo(BeNil())
+			})
+
+			It("Should not contain Mailroom resources", func() {
+				Expect(enabled).To(BeFalse())
+				Expect(deployment).To(BeNil())
+				Expect(configMap).To(BeNil())
+				Expect(hpa).To(BeNil())
+				Expect(networkPolicy).To(BeNil())
+				Expect(serviceAccount).To(BeNil())
+			})
+
+		})
+	})
+})

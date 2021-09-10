c0776e70709414e870091bf4acca8e78edc4e9e7
Gerard Hickey <ghickey@gitlab.com>
2021-09-10 13:24:21 -0700

    Added dumpTemplateToFile()

--- controllers/gitlab/test_utils.go
+++ controllers/gitlab/test_utils.go
@@ -2,12 +2,11 @@ package gitlab
 
 import (
 	"os"
-	"syscall"
+	"strings"
 
 	gitlabv1beta1 "gitlab.com/gitlab-org/cloud-native/gitlab-operator/api/v1beta1"
 	"gitlab.com/gitlab-org/cloud-native/gitlab-operator/helm"
 
-	//corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -34,12 +33,27 @@ func createMockAdapter(namespace string, version string, values helm.Values) Cus
 }
 
 // dumpTemplate() will serialize the template and display the YAML for debugging
-func dumpTemplate(adapter CustomResourceAdapter) {
-	stdout := os.NewFile(uintptr(syscall.Stdout), "/dev/stdout")
+func dumpTemplate(template helm.Template) string {
+	output := new(strings.Builder)
+
 	s := json.NewYAMLSerializer(json.DefaultMetaFactory, scheme.Scheme, scheme.Scheme)
-	template, _ := GetTemplate(adapter)
 	for _, o := range(template.Objects()) {
-		stdout.WriteString("---\n")
-		s.Encode(o, stdout)
+		output.WriteString("---\n")
+		s.Encode(o, output)
 	}
+
+	return output.String()
+}
+
+// dumpTemplateToFile() will output the Helm template to a file
+// Note: the file is written to where the test runs NOT from where the
+//       tests were run from
+func dumpTemplateToFile(template helm.Template, filename string) error {
+	fh, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	fh.WriteString(dumpTemplate(template))
+	fh.Close()
+	return nil
 }

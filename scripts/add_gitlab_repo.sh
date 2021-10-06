5b3f76881cfc5e0be0188a2fedbbe3a64e539a39
Balasankar "Balu" C <balasankar@gitlab.com>
2021-10-06 12:01:58 +0530

    Support fetching charts from dev.gitlab.org registry
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- scripts/add_gitlab_repo.sh
+++ scripts/add_gitlab_repo.sh
@@ -1,16 +1,26 @@
 #!/bin/sh -e
 
-GITLAB_HELM_REPO="https://charts.gitlab.io/"
 HELM="bin/helm"
-
 scripts_dir="$(dirname "$0")"
+GITLAB_HELM_REPO="https://charts.gitlab.io/"
+HELM_ARGS=""
 
+is_dev_mirror() {
+    [ "$CI_PROJECT_PATH" = "gitlab/cloud-native/gitlab-operator" ]
+}
 
 add_gitlab_repo() {
     echo "Adding ${GITLAB_HELM_REPO} to list of helm repos"
-    $HELM repo list | grep -q '^gitlab' || $HELM repo add gitlab ${GITLAB_HELM_REPO}
+    $HELM repo list | grep -q '^gitlab' || $HELM repo add ${HELM_ARGS} gitlab ${GITLAB_HELM_REPO}
     $HELM repo update
 }
 
 . "${scripts_dir}/install_helm.sh"
+
+if is_dev_mirror; then
+    CHANNEL=${CHANNEL:-stable}
+    GITLAB_HELM_REPO="${CI_API_V4_URL}/projects/${DEV_CHARTS_PROJECT_ID}/packages/helm/${CHANNEL}"
+    HELM_ARGS="--username ${DEV_CHARTS_USERNAME} --password ${DEV_CHARTS_PAT}"
+fi
+
 add_gitlab_repo

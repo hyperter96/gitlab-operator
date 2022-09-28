dae15a9376744096abd8c9e3ebb0c1963d4daf51
Balasankar 'Balu' C <balasankar@gitlab.com>
2022-09-28 02:20:57 +0000

    Retry fetching charts on failure
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- scripts/retrieve_gitlab_charts.sh
+++ scripts/retrieve_gitlab_charts.sh
@@ -8,6 +8,8 @@
 
 HELM="bin/helm"
 GITLAB_CHART="gitlab/gitlab"
+MAX_CHART_FETCH_ATTEMPTS=${MAX_CHART_FETCH_ATTEMPTS:-100}
+CHART_FETCH_WAIT_TIME=${CHART_FETCH_WAIT_TIME:-30s}
 
 scripts_dir="$(dirname "$0")"
 . "${scripts_dir}/add_gitlab_repo.sh"
@@ -15,6 +17,16 @@ scripts_dir="$(dirname "$0")"
 # Download the charts to the charts directory
 rm -rf charts && mkdir charts
 for version in $(cat CHART_VERSIONS); do
+    count=0
     echo "Fetching ${GITLAB_CHART}-${version}"
-    $HELM fetch "${GITLAB_CHART}" --version "${version}" --destination ./charts/
+    while ! $($HELM fetch "${GITLAB_CHART}" --version "${version}" --destination ./charts/ 2> /dev/null); do
+        if [ $count -ge "${MAX_CHART_FETCH_ATTEMPTS}" ]; then
+            echo "  Fetch attempts exhausted. Exiting."
+            exit 1;
+        fi
+
+        echo "  Could not fetch chart. Sleeping for ${CHART_FETCH_WAIT_TIME} before attempting again."
+        sleep "${CHART_FETCH_WAIT_TIME}"
+        count=$((count+1))
+    done
 done

4ecd00dab7a5abab3d3d70a32eb11e3490e0cd63
Mitchell Nielsen <mnielsen@gitlab.com>
2023-03-07 10:09:34 +0000

    Automatically calculate previous version
    Uses git to automatically find the previous tag so it does
    not need to be entered manually.

--- scripts/generate_release_message.sh
+++ scripts/generate_release_message.sh
@@ -1,64 +1,64 @@
-#!/bin/bash
+#!/bin/sh
 
 # This script generates the message to be placed in
 # the description of a Release.
 #
-# This is meant to be used after the Release has been created via CI.
-#
 # It provides:
-#   1. A link to compare the changes between the previous
-#      version and the target version.
+#   1. A link to the changelog.
 #   2. A table mapping each supported Chart version to its
 #      corresponding GitLab version.
 #
+# Dependencies:
+# - git
+# - helm
+#
 # Usage:
-#   ./generate_release_message.sh <previous version> <target version>
+#   ./generate_release_message.sh <target version>
 
-set -eo pipefail
+set -e
 
-previousVersion="${1:-none}"
-version="${2:-none}"
+version="${1:-none}"
 
-function usage() {
-  if [ "${version}" = 'none' ] || [ "${previousVersion}" = 'none' ]; then
-    echo 'Usage: ./script <previous version> <target version>'
+usage() {
+  if [ "${version}" = 'none' ]; then
+    echo 'Usage: ./script <target version>'
     exit 1
   fi
 }
 
-function get_versions_from_message() {
+get_versions_from_message() {
   tag_message=$(git show "${version}" --quiet | grep 'supports GitLab Charts')
 
   echo "${tag_message}" | awk '{print $7 " " $8 " " $9}' | sed 's/,//g'
 }
 
-function find_gitlab_version() {
+find_gitlab_version() {
   chart_version="${1}"
 
   helm repo update > /dev/null 2>&1
 
-  helm search repo gitlab/gitlab -l -o json \
-    | jq -r --arg version "${chart_version}" \
-        '.[] | select(.name=="gitlab/gitlab") | select(.version==$version) | .app_version'
+  helm search repo gitlab/gitlab -l -o table \
+    | grep "${chart_version}" \
+    | awk '{print $3}'
 }
 
-function get_version_map() {
+get_version_map() {
   for tag in $(get_versions_from_message); do
     gitlabVersion=$(find_gitlab_version "${tag}")
     printf "%s | %s\n" "${tag}" "${gitlabVersion}"
   done
 }
 
-function generate() {
+generate() {
   printf '
 ## Changelog
-[Changes](https://gitlab.com/gitlab-org/cloud-native/gitlab-operator/-/compare/%s...%s?from_project_id=18899486)
+[CHANGELOG.md](https://gitlab.com/gitlab-org/cloud-native/gitlab-operator/-/blob/master/CHANGELOG.md)
 
 ## Version mapping
 Chart version | GitLab version
 -|-
 %s
-' "${previousVersion}" "${version}" "$(get_version_map)"
+' "$(get_version_map)"
 }
 
 usage
